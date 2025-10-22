e484d92431e68b5cf574ed71a0cd2ee0ce3d18d6
rama-alatiq <rama.alateeqq@gmail.com>
2025-10-22 16:22:16 +0300

    configure database connection and update FastAPI root path

--- todoApp/db/db.py
+++ todoApp/db/db.py
@@ -1,11 +1,22 @@
-
+import os
+from sqlalchemy import NullPool
 from sqlmodel import SQLModel, Session, create_engine
 
 
-sqlite_file_name="todo_database.db"
-sqlite_url=f"sqlite:///{sqlite_file_name}"
-connect_args={"check_same_thread":False}
-engine=create_engine(sqlite_url,connect_args=connect_args)
+# sqlite_file_name="todo_database.db"
+# sqlite_url=f"sqlite:///{sqlite_file_name}"
+# connect_args={"check_same_thread":False}
+# engine=create_engine(sqlite_url,connect_args=connect_args)
+
+database_url=os.environ.get(
+    "DATABASE_URL", 
+    "sqlite:///./fallback.db" 
+)
+engine=create_engine(
+    database_url,
+    echo=False,
+    poolclass=NullPool
+)
 
 def create_db_and_table():
     SQLModel.metadata.create_all(engine)
