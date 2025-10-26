cf7af5f35fde21ef2ced5971ac70b4f33a301d83
rama-alatiq <rama.alateeqq@gmail.com>
2025-10-26 09:12:25 +0300

    refactor: comment out unused database session handling code

--- todoApp/main.py
+++ todoApp/main.py
@@ -1,23 +1,23 @@
-from contextlib import asynccontextmanager
-from datetime import datetime
-from fastapi import FastAPI
-from sqlmodel import Session, select
-from todoApp.db.db import engine
-from todoApp.models.models import TodoItem
 from todoApp.routers.todos import router as todos_router
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+# from datetime import datetime
+# from sqlmodel import Session, select
+# from todoApp.db.db import engine
+# from todoApp.models.models import TodoItem
 
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     # create_db_and_table()
-    with Session(engine) as session:
-        if not session.exec(select(TodoItem)).first():
-            session.add_all([
-                TodoItem(title="First Task",description="This is the first task",due_date=datetime.now()),
-                TodoItem(title="Second Task",description="This is the second task",due_date=datetime.now()),
-            ])
-            session.commit()
-            
+    # with Session(engine) as session:
+        # if not session.exec(select(TodoItem)).first():
+        #     session.add_all([
+        #         TodoItem(title="First Task",description="This is the first task",due_date=datetime.now()),
+        #         TodoItem(title="Second Task",description="This is the second task",due_date=datetime.now()),
+        #     ])
+        #     session.commit()
+        
     yield
 
 app = FastAPI(lifespan=lifespan)
